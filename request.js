e2e25d8d4520b3fb764ad9a7261156baaf9d8c64
Cemus <24947582+Cemus@users.noreply.github.com>
2024-10-27 22:05:05 +0100

    Add herculito 4

---
+++ request.js
@@ -0,0 +1,38 @@
+function fetchPy() {
+  fetch("https://pydefis.callicode.fr/defis/ExempleURL/get/Euthyphron/66eed")
+    .then((response) => {
+      return response.text();
+    })
+    .then((data) => {
+      console.log(data);
+      const splittedData = data.split("\n");
+      const signature = splittedData[0];
+      const total = parseInt(splittedData[1]) + parseInt(splittedData[2]);
+      const result = `sig:${signature}\nrep:${total}`;
+      console.log(result);
+      sendPy(result);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+function sendPy(result) {
+  fetch("https://pydefis.callicode.fr/defis/ExempleURL/post/Euthyphron/66eed", {
+    method: "POST",
+    headers: {
+      "Content-Type": "text/plain",
+    },
+    body: result,
+  })
+    .then((response) => {
+      return response.text();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+fetchPy();
