b4b6c0e5bc2cb02934f154686dfbff845cc42e17
Cemus <24947582+Cemus@users.noreply.github.com>
2024-10-31 11:18:43 +0100

    Add meli_melo

--- level_2/meli_melo.js
+++ level_2/meli_melo.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 
+const solution = "1a6e1g1i1l1m1n1o2r2s2u1y";
+
 async function readAddresses() {
   try {
     const data = await fs.promises.readFile("C24_adresses.txt");
@@ -9,7 +11,52 @@ async function readAddresses() {
   }
 }
 
-let addresses = readAddresses().toString();
+function getPrint(address) {
+  const letters = [];
 
-addresses.split("\n");
-console.log(addresses);
+  for (let i = 0; i < address.length; i++) {
+    letters.push(address[i]);
+  }
+
+  letters.sort();
+  const print = [];
+  let prevLetter = "";
+  let letterCount = 0;
+
+  for (let i = 0; i <= letters.length; i++) {
+    if (letters[i] !== prevLetter) {
+      if (prevLetter !== "") {
+        print.push(letterCount, prevLetter);
+      }
+      letterCount = 1;
+      prevLetter = letters[i];
+    } else {
+      letterCount++;
+    }
+  }
+  const result = { address: address, print: print.join("") };
+  return result;
+}
+
+async function main() {
+  let addresses = await readAddresses();
+
+  const prints = [];
+  const addressArray = addresses.split("\n");
+
+  addressArray.forEach((address) => {
+    let optiAdress = address.replaceAll(" ", "").replaceAll("-", "");
+    prints.push(getPrint(optiAdress));
+  });
+
+  let result = "";
+  for (const [key, value] of Object.entries(prints)) {
+    if (value.print === solution) {
+      console.log(value.address);
+      result = value.address;
+    }
+  }
+  console.log(result);
+}
+
+main();
